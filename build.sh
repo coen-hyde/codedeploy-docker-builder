68bff7b90168f02818add7262a1f3c1938d259bc
Coen Hyde <coen.hyde@gmail.com>
2015-11-20 15:19:50 -0800

    move s3 PUT to release script

--- build.sh
+++ build.sh
@@ -22,30 +22,6 @@ main() {
   if [[ $BUILDKITE_TAG != "latest" ]]; then
     docker push $DOCKER_IMAGE:latest
   fi
-
-  cp $image_dir/deployment/production.yml $build_dir/release/production.yml
-
-  cd $build_dir
-
-  cat <<EOF >./release/config.json
-{
-  "docker": {
-    "registry": "$DOCKER_REGISTRY",
-    "image": "$DOCKER_IMAGE:$BUILDKITE_TAG",
-    "login_email": "$DOCKER_LOGIN_EMAIL",
-    "login_username": "$DOCKER_LOGIN_USERNAME",
-    "login_password": "$DOCKER_LOGIN_PASSWORD"
-  }
-}
-EOF
-
-  # Package release
-  $(cd release && tar -cf ../release.tar ./)
-
-  # Put release on S3
-  echo "Pushing release to s3://$RELEASES_BUCKET/release-${BUILDKITE_TAG}.tar"
-  echo "aws s3api put-object --bucket=\"$RELEASES_BUCKET\" --key=\"release-${BUILDKITE_TAG}.tar\" --body release.tar"
-  aws s3api put-object --bucket="$RELEASES_BUCKET" --key="release-${BUILDKITE_TAG}.tar" --body=release.tar
 }
 
 main "$@"
--- release.sh
+++ release.sh
@@ -3,9 +3,36 @@
 set -eo
 
 main() {
+  local image_dir=/usr/src/image-2-build
+  local build_dir=/usr/src/docker-builder
+
   if [[ ! $BUILDKITE_TAG ]]; then
-    BUILDKITE_TAG=latest
+    BUILDKITE_TAG="latest"
   fi
+
+  cp $image_dir/deployment/production.yml $build_dir/release/production.yml
+
+  cd $build_dir
+
+  cat <<EOF >./release/config.json
+{
+  "docker": {
+    "registry": "$DOCKER_REGISTRY",
+    "image": "$DOCKER_IMAGE:$BUILDKITE_TAG",
+    "login_email": "$DOCKER_LOGIN_EMAIL",
+    "login_username": "$DOCKER_LOGIN_USERNAME",
+    "login_password": "$DOCKER_LOGIN_PASSWORD"
+  }
+}
+EOF
+
+  # Package release
+  $(cd release && tar -cf ../release.tar ./)
+
+  # Put release on S3
+  echo "Pushing release to s3://$RELEASES_BUCKET/release-${BUILDKITE_TAG}.tar"
+  echo "aws s3api put-object --bucket=\"$RELEASES_BUCKET\" --key=\"release-${BUILDKITE_TAG}.tar\" --body release.tar"
+  aws s3api put-object --bucket="$RELEASES_BUCKET" --key="release-${BUILDKITE_TAG}.tar" --body=release.tar
 
   # Register revision with code deploy
   local revision=$(cat <<EOF
