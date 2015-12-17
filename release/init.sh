a19de07842bbb5405ab19e1fe9e838d077d73ad5
Coen Hyde <coen.hyde@gmail.com>
2015-12-17 12:53:14 -0800

    set awslogs_stream in docker compose yml file

--- release/init.sh
+++ release/init.sh
@@ -11,9 +11,15 @@ main() {
   local docker_login_username="$(echo $config | jq -r '.docker.login_username')"
   local docker_login_password="$(echo $config | jq -r '.docker.login_password')"
   local docker_compose_file="$release_dir/production.yml"
+  local ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+  local deployment_id="$(cd ${release_dir}/../ && basename $PWD)"
+  local awslogs_stream="${ip}-${deployment_id}"
 
   # modify docker-compose production file with the image release
   sed -i -e "s/{{release}}/$(echo $docker_image | sed -e 's/[\/&]/\\&/g')/" $docker_compose_file
+
+  # set the awslog stream
+  sed -i -e "s/{{awslogs_stream}}/$(echo "${awslogs_stream}" | sed -e 's/[\/&]/\\&/g')/" $docker_compose_file
 
   docker login --email="${docker_login_email}" --username="${docker_login_username}" --password="${docker_login_password}" $docker_registry
   docker pull $docker_image
