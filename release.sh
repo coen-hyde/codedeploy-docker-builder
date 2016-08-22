05af7bed5a69bc86cbc6d96ac00d46ac948c324e
Coen Hyde <coen.hyde@gmail.com>
2016-08-21 23:17:10 -0700

    use BUILDKITE_BUILD_ID instead of BUILDKITE_TAG for build id

--- release.sh
+++ release.sh
@@ -6,10 +6,6 @@ main() {
   local image_dir=/usr/src/image-2-build
   local build_dir=/usr/src/docker-builder
 
-  if [[ ! $BUILDKITE_TAG ]]; then
-    BUILDKITE_TAG="latest"
-  fi
-
   cd $image_dir
 
   cp docker-compose/production.yml $build_dir/release/production.yml
@@ -20,7 +16,7 @@ main() {
 {
   "docker": {
     "registry": "$DOCKER_REGISTRY",
-    "image": "$DOCKER_IMAGE:$BUILDKITE_TAG",
+    "image": "$DOCKER_IMAGE:$BUILDKITE_BUILD_ID",
     "login_email": "$DOCKER_LOGIN_EMAIL",
     "login_username": "$DOCKER_LOGIN_USERNAME",
     "login_password": "$DOCKER_LOGIN_PASSWORD"
@@ -32,9 +28,9 @@ EOF
   $(cd release && tar -cf ../release.tar ./)
 
   # Put release on S3
-  echo "Pushing release to s3://$RELEASES_BUCKET/release-${BUILDKITE_TAG}.tar"
-  echo "aws s3api put-object --bucket=\"$RELEASES_BUCKET\" --key=\"release-${BUILDKITE_TAG}.tar\" --body release.tar"
-  aws s3api put-object --bucket="$RELEASES_BUCKET" --key="release-${BUILDKITE_TAG}.tar" --body=release.tar
+  echo "Pushing release to s3://$RELEASES_BUCKET/release-${BUILDKITE_BUILD_ID}.tar"
+  echo "aws s3api put-object --bucket=\"$RELEASES_BUCKET\" --key=\"release-${BUILDKITE_BUILD_ID}.tar\" --body release.tar"
+  aws s3api put-object --bucket="$RELEASES_BUCKET" --key="release-${BUILDKITE_BUILD_ID}.tar" --body=release.tar
 
   # Register revision with code deploy
   local revision=$(cat <<EOF
@@ -42,7 +38,7 @@ EOF
   "revisionType": "S3",
   "s3Location": {
     "bucket": "$RELEASES_BUCKET",
-    "key": "release-${BUILDKITE_TAG}.tar",
+    "key": "release-${BUILDKITE_BUILD_ID}.tar",
     "bundleType": "tar"
   }
 }
@@ -53,7 +49,7 @@ EOF
     --application-name="$CD_APPLICATION_NAME" \
     --deployment-group-name="$CD_DEPLOYMENT_GROUP_NAME" \
     --deployment-config-name CodeDeployDefault.OneAtATime \
-    --description="release: $BUILDKITE_TAG" \
+    --description="release: $BUILDKITE_BUILD_ID" \
     --revision="$revision"
 }
 
