b34f2f6d9bcaaa4adfca25c06ec0a5622ce8693e
Coen Hyde <coen.hyde@gmail.com>
2016-09-18 15:19:01 -0700

    allow application 20 seconds to stop

--- release/stop_service.sh
+++ release/stop_service.sh
@@ -6,7 +6,7 @@ main() {
   local release_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   local docker_compose_file="$release_dir/production.yml"
 
-  docker-compose -f $docker_compose_file stop
+  docker-compose -f $docker_compose_file stop -t 20
   docker-compose -f $docker_compose_file rm
 }
 
