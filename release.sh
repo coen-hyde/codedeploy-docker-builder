88a12102c87651d0280ca4bd23108f00e2ee063d
Coen Hyde <coen.hyde@gmail.com>
2016-10-13 14:10:52 -0700

    use env DOCKER_IMAGE with docker version for tag

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 main() {
   local image_dir=/usr/src/image-2-build
   local build_dir=/usr/src/docker-builder
+  local tag="${DOCKER_IMAGE##*:}"
 
   cd $image_dir
 
@@ -16,13 +17,17 @@ main() {
     APPLICATION_PORT=3000
   fi
 
+  if [[ ! $CODEDEPLOY_CONFIG_NAME ]]; then
+    CODEDEPLOY_CONFIG_NAME=CodeDeployDefault.OneAtATime
+  fi
+
   cat <<EOF >./release/config.json
 {
   "release_bucket": "$RELEASES_BUCKET",
   "port": "$APPLICATION_PORT",
   "docker": {
     "registry": "$DOCKER_REGISTRY",
-    "image": "$DOCKER_IMAGE:$DOCKER_TAG",
+    "image": "$DOCKER_IMAGE",
     "login_email": "$DOCKER_LOGIN_EMAIL",
     "login_username": "$DOCKER_LOGIN_USERNAME",
     "login_password": "$DOCKER_LOGIN_PASSWORD"
@@ -34,20 +39,20 @@ EOF
   $(cd release && tar -cf ../release.tar ./)
 
   # Put release on S3
-  echo "Pushing release to s3://$RELEASES_BUCKET/release-${DOCKER_TAG}.tar"
-  echo "aws s3api put-object --bucket=\"$RELEASES_BUCKET\" --key=\"release-${DOCKER_TAG}.tar\" --body release.tar"
+  echo "Pushing release to s3://${RELEASES_BUCKET}/release-${tag}.tar"
+  echo "aws s3api put-object --bucket=\"${RELEASES_BUCKET}\" --key=\"release-${tag}.tar\" --body release.tar"
 
   if [[ $KMS_KEY_ID ]]; then
     aws s3api put-object \
-      --bucket="$RELEASES_BUCKET" \
-      --key="release-${DOCKER_TAG}.tar" \
+      --bucket="${RELEASES_BUCKET}" \
+      --key="release-${tag}.tar" \
       --server-side-encryption "aws:kms" \
       --ssekms-key-id "$KMS_KEY_ID" \
       --body=release.tar
   else
     aws s3api put-object \
       --bucket="$RELEASES_BUCKET" \
-      --key="release-${DOCKER_TAG}.tar" \
+      --key="release-${tag}.tar" \
       --body=release.tar
   fi
 
@@ -57,7 +62,7 @@ EOF
   "revisionType": "S3",
   "s3Location": {
     "bucket": "$RELEASES_BUCKET",
-    "key": "release-${DOCKER_TAG}.tar",
+    "key": "release-${tag}.tar",
     "bundleType": "tar"
   }
 }
@@ -65,11 +70,11 @@ EOF
 )
 
   local deployment=$(aws deploy create-deployment \
-    --application-name="$CD_APPLICATION_NAME" \
-    --deployment-group-name="$CD_DEPLOYMENT_GROUP_NAME" \
-    --deployment-config-name CodeDeployDefault.OneAtATime \
-    --description="release: $DOCKER_TAG" \
-    --revision="$revision")
+    --application-name="${CD_APPLICATION_NAME}" \
+    --deployment-group-name="${CD_DEPLOYMENT_GROUP_NAME}" \
+    --deployment-config-name="${CODEDEPLOY_CONFIG_NAME}" \
+    --description="release: ${tag}" \
+    --revision="${revision}")
 
   local deployment_id="$(echo "$deployment" | jq -r '.deploymentId')"
 
